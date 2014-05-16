86a75f46e376b943760a2b11cfb148430ef117f2
Vignesh Jayavel <vignesh.j@freshdesk.com>
2014-05-16 11:41:38 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+#source :gemcutter
+source 'http://rubygems.org'
+
+gem "rake", "0.8.7"
+gem "rails","2.3.18"
+gem "mysql2", "~> 0.2.7", :platforms => :ruby_19
+
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'b2804d59604d268a8a69dad453df96f1674585e42b3b0ef6776a77e05cdfec243a04d927b6100a9f53ece4e9fb472107622c0ce919f60dcbec9f66addd75a6c3';
