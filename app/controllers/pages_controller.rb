9d59026fa6522c7d426479292d60808e8c860a43
Vignesh Jayavel <vignesh.j@freshdesk.com>
2014-05-16 13:24:20 +0530

    done with the static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def contacts
+  	@title = "Contacts"
   end
 
   def about
+  	@title = "About"
   end
 
   def team
+  	@title = "Team"
   end
 
 end
