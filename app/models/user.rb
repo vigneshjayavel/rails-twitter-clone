4bafc532a909276ba7dcae214507e594cca28edd
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-05-20 20:18:44 +0530

    added password to User model and password validation logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,48 @@
+require "digest"
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
 	
 	EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates_presence_of :name, :email	
 	validates_length_of :name, :maximum => 50
 	validates_format_of :email, :with => EmailRegex
 	validates_uniqueness_of :email, :case_sensitive => false
+
+	#Automatically create a virtual attribute passoword:confirmation
+	validates_confirmation_of :password
+
+	#Password validation
+	validates_presence_of :password
+	validates_length_of :password, :within => 6..40
+
+	before_save :encrypt_password
+
+	#check whether the entered password matches the password in the record
+	def has_password?(submitted_password)
+		#compare encrypted password with the encrypted version of the submitted_password
+		encrypted_password == encrypt(submitted_password)
+	end
+
+	private
+		def encrypt_password
+			self.salt = make_salt
+			self.encrypted_password = encrypt self.password
+		end
+
+		def encrypt(string)
+			secure_hash "#{salt}#{string}"
+		end
+
+		def make_salt
+			secure_hash "#{Time.now.utc}#{password}"
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest string
+		end
+	
+
+	
 
 end
