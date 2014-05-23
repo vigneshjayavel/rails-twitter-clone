743bc9f3017e975407ffa709be5d674f74ecc888
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-05-23 12:20:22 +0530

    display all users without pagination

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ ActionController::Routing::Routes.draw do |map|
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.signin '/signin', :controller => 'sessions', :action => 'new'
   map.signout '/signout', :controller => 'sessions', :action => 'destroy'
+  map.users 'users', :controller => 'users', :action => 'index'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
