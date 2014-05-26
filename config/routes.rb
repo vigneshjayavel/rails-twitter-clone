b52b253a1e56778a87aa104f35d6010f1f7ee15f
Vignesh Jayavel <vigneshjayavel@freshdesk.com>
2014-05-26 13:14:53 +0530

    added user followers stats partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sessions, :only => [:new, :create, :destroy]
   map.resources :users
   map.resources :microposts, :only => [:create, :destroy]
+  map.resources :users, :member => {:following => :get, :followers => :get}
   map.home '/home', :controller => 'pages', :action => 'home'
   map.about '/about', :controller => 'pages', :action => 'about'
   map.contacts '/contacts', :controller => 'pages', :action => 'contacts'
